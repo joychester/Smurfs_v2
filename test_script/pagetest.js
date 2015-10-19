cd51d113e89709f708feb044827646ae8221ce36
joychester <joychester@gmail.com>
2015-10-19 12:04:30 +0800

    modify the test script

--- test_script/pagetest.js
+++ test_script/pagetest.js
@@ -14,30 +14,33 @@ function *run() {
 
   var nightmare = Nightmare({'port': freeport, 'timeout': 15000, 'interval': 200});
   yield nightmare
+    //load landing page
     .goto(test_url)
-	  // .inject("js","xhr.js") 
+	  // .inject("js","xhr.js")
     .wait('.event-title')
     .evaluate(function(tp){
        var tag = 'artist_page';
-		   var loadtiming =  new Date().getTime()-window.performance.timing.navigationStart;	
+       var starttime = window.performance.timing.navigationStart;
+		   var loadtiming =  Date.now()-starttime;
+       //make ajax call to kafka server to save the timing
 	     var xhr = new XMLHttpRequest();
 	     xhr.open( "post", "http://localhost:8887/postkf", false);
-		   xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");  
-	     xhr.send('client_id='+tag+'&topic='+tp+'&msg='+loadtiming);	    
-	     }, tp) // end of evaluate
+		   xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
+	     xhr.send('client_id='+tag+'&topic='+tp+'&msg='+loadtiming);
+	     }, tp)
 
     //load event page
     .click('.event-title')
     .wait('#sell-tickets')
     .evaluate(function(tp){
        var tag = 'event_page';
-       var loadtiming =  new Date().getTime()-window.performance.timing.navigationStart;	
+       var starttime = window.performance.timing.navigationStart;
+       var loadtiming =  Date.now()-starttime;
+       //make ajax call to kafka server to save the timing
        var xhr = new XMLHttpRequest();
        xhr.open( "post", "http://localhost:8887/postkf", false);
-       xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");  
+       xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
        xhr.send('client_id='+tag+'&topic='+tp+'&msg='+loadtiming);
-    }, tp) // end of evaluate
-  //yield nightmare.end();
+    }, tp)
     .end();
 }
-
