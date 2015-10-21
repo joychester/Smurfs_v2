dd0aaede5950a0916e651971277fd9e6df0ff5e3
joychester <joychester@gmail.com>
2015-10-21 15:52:34 +0800

    use better timing in xhr.js

--- xhr.js
+++ xhr.js
@@ -1,6 +1,5 @@
 function send_message(tp,tag){
-	   var starttime = window.performance.timing.navigationStart;
-       var loadtiming =  Date.now()-starttime;
+			 var loadtiming = window.performance.now();
        //make ajax call to kafka server to save the timing
        var xhr = new XMLHttpRequest();
        xhr.open( "post", "http://localhost:8887/postkf", false);
