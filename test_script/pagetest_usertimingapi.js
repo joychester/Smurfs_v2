8dbe2641947f12d68b12a7619b7e79233d772dfc
joychester <joychester@gmail.com>
2016-02-01 11:05:35 +0800

    add sample script for user timing API

---
+++ test_script/pagetest_usertimingapi.js
@@ -0,0 +1,48 @@
+var Nightmare = require('nightmare');
+var vo = require('vo');
+
+//tp is used for collect data by kafka, you can ignore it
+var test_url = "https://www.etsy.com/", tp = 'test2';
+//const RENDER_TIME_MS = 1000;
+
+vo(run)(function(err, result) {
+  if (err) throw err;
+});
+
+function *run() {
+  var user_timing = 0; 
+  var nightmare = Nightmare({'waitTimeout': 15000, show: true});
+
+  var user_time = yield nightmare
+    //load landing page
+    .goto(test_url)
+    //.wait('.event-title')
+    //.wait(RENDER_TIME_MS)
+    .wait(function(){
+      var marks = window.performance.getEntriesByType("mark");
+      if (marks.length > 0) {
+          for (var m of marks ) {
+            if (m.name === "timer_loadend_base") {
+              return true;
+            } else {
+              continue;
+            }
+          }
+          return false;
+      } else {
+        return false;
+      }
+    })
+    .evaluate(function(){
+      var marks = window.performance.getEntriesByType("mark");
+        for (var m of marks ) {
+          if (m.name === "timer_loadend_base") {
+            return m.startTime.toFixed(1);
+          }
+        }
+        return "No such mark..."
+    });
+
+    console.log(user_time);
+    yield nightmare.end();
+}
